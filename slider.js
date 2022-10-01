1f038cfb9b740471ff5360aa63abd30afe6c9ffd
agustinff99 <agustin99ff@gmail.com>
2022-09-30 23:05:09 -0300

    SegundaVersion_30/09/22

--- slider.js
+++ slider.js
@@ -1,31 +1,16 @@
-(function(){
-    
-  const sliders = [...document.querySelectorAll('.productos_body')];
-  const buttonNext = document.querySelector('#next');
-  const buttonBefore = document.querySelector('#before');
-  let value;   
+const productosContainers = [...document.querySelectorAll('.productos_container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-  buttonNext.addEventListener('click', ()=>{
-      changePosition(1);
-  });
+productosContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
 
-  buttonBefore.addEventListener('click', ()=>{
-      changePosition(-1);
-  });
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
 
-  const changePosition = (add)=>{
-      const currentTestimony = document.querySelector('.productos_body--show').dataset.id;
-      value = Number(currentTestimony);
-      value+= add;
-
-
-      sliders[Number(currentTestimony)-1].classList.remove('productos_body--show');
-      if(value === sliders.length+1 || value === 0){
-          value = value === 0 ? sliders.length  : 1;
-      }
-
-      sliders[value-1].classList.add('productos_body--show');
-
-  }
-
-})();
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
